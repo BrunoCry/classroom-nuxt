9e80f51b5a3d37a829e5e79f064cc4ab2af6718c
BrunoCry <danil-az2@mail.ru>
2022-06-30 03:27:34 +0300

    upload project

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Button label="Submit">
+            123
+        </Button>
+    </div>
+</template>
+
+<script>
+    import Sidebar from 'primevue/sidebar';
+    import Button from 'primevue/button';
+
+    export default {
+        components: {
+            Sidebar, Button
+        },
+        name: 'IndexPage',
+
+        data () {
+            return {
+                sidebar: true
+            }
+        }
+    }
+</script>
